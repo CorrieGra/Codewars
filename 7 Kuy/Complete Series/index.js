c519603ac8723729ee87fd088bb4be0804a3a54f
Corrie Graham <corrie@Corries-MacBook-Air.local>
2020-07-07 20:35:22 +0200

    completed kata

--- 7 Kuy/Complete Series/index.js
+++ 7 Kuy/Complete Series/index.js
@@ -1,3 +1,19 @@
 function completeSeries(arr) {
     // write your code here
-  }
+    arr.sort((a, b) => a < b ? -1 : 1);
+
+    const hasDuplicate = arr.filter((v, i) => arr.indexOf(v) !== i).length > 0 ? true : false;
+
+    if (hasDuplicate) return [0];
+
+    let result = [];
+    let i = 0;
+    const maxInt = Math.max(...arr);
+
+    for (i; i <= maxInt; i++)
+    {
+        result.push(i);
+    }
+
+    return result;
+}
