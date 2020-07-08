f426bde5779598a9350fdd42f82792b97f39be01
Corrie Graham <corrie@Corries-MacBook-Air.local>
2020-07-08 18:53:57 +0200

    improved solution

--- 6 Kyu/Multiples of 3 or 5/index.js
+++ 6 Kyu/Multiples of 3 or 5/index.js
@@ -1,9 +1,5 @@
 function solution(n){
-    let i = 1;
-      let multiples = [];
-      for (i; i < n; i++) {
-          i % 3 === 0 || i % 5 === 0 ? multiples.push(i) : null;
-      }
-  
-      return multiples.length === 0 ? 0 : multiples.reduce((acc, curr) => acc + curr);
-  }
+    if ( typeof n === 'undefined' || n <= 1) return 0;
+  let arr = [...Array(n - 1).keys()].map((v) => (v + 1));
+  return arr.length !== 0 ? arr.filter(v => v % 5 === 0 || v % 3 === 0).reduce((acc, curr) => acc + curr, 0) : 0;
+}
