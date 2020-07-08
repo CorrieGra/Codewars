1a9e61f82f4d201f7f5a49eb2a883f95b9a0a42c
Corrie Graham <corrie@Corries-MacBook-Air.local>
2020-07-08 18:59:52 +0200

    added kata

--- 6 Kyu/Multiples of 3 or 5/index.js
+++ 6 Kyu/Multiples of 3 or 5/index.js
@@ -1,5 +1,5 @@
 function solution(n){
-    if ( typeof n === 'undefined' || n <= 1) return 0;
+   if ( typeof n === 'undefined' || n <= 1) return 0;
   let arr = [...Array(n - 1).keys()].map((v) => (v + 1));
   return arr.length !== 0 ? arr.filter(v => v % 5 === 0 || v % 3 === 0).reduce((acc, curr) => acc + curr, 0) : 0;
 }
