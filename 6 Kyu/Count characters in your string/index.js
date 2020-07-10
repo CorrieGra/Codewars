dac677e2fef765ebdd349e5c3975cd4e12cee433
Corrie Graham <corrie@Corries-MacBook-Air.local>
2020-07-10 11:44:36 +0200

    completed solution & added simpler solution

--- 6 Kyu/Count characters in your string/index.js
+++ 6 Kyu/Count characters in your string/index.js
@@ -1,4 +1,22 @@
-function count (string) {  
-    // The function code should be here
-     return {};
-  }
+// OVERCOMPLICATED IT !!!
+function count(_string = '') {
+    // The function code should be here;
+    let result = {};
+    let _set = new Set(_string);
+
+    _string.split('').forEach(v => {
+        if (_set.has(v) && !result[v]) return result[v] = 1;
+        if (_set.has(v) && result[v]) return ++result[v];
+    });
+
+    return result;
+}
+
+// SIMPLET SOLUTION
+function count2(_string){
+    const result = {};
+    _string.split('').forEach(v => result[v] ? result[v]++ : result[v] = 1);
+    return result;
+}
+
+console.log(count2('aba'), 2);
