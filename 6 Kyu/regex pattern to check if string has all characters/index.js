500bdcfd5484ff12f8a67329fc5c3253ffcc16a3
Corrie Graham <corrie@Corries-MacBook-Air.local>
2020-07-13 09:52:07 +0200

    completed kata

--- 6 Kyu/regex pattern to check if string has all characters/index.js
+++ 6 Kyu/regex pattern to check if string has all characters/index.js
@@ -1,9 +1,4 @@
 function regexContainsAll(str) {
-    
-  }
-
-  console.log(regexContainsAll('abc').test('abc'), 'EXPECTED : true' , regexContainsAll('abc'));
-  console.log(regexContainsAll('abc').test('bca'), 'EXPECTED : true' , regexContainsAll('abc'));
-  console.log(regexContainsAll('abc').test('ba'), 'EXPECTED : false' , regexContainsAll('abc'));
-  console.log(regexContainsAll('abc').test('baczzz'), 'EXPECTED : true' , regexContainsAll('abc'));
-  console.log(regexContainsAll('dL').test('ZQRdLE'),  'EXPECTED : true' ,regexContainsAll('dL'));
+  let regEx = str.split('').map(v => `(?=.*[${v}])`).join('');
+  return new RegExp(`^${regEx}`);
+}
