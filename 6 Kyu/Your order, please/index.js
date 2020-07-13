786bcf411e34ed3f9719989475266f07313b881f
Corrie Graham <corrie@Corries-MacBook-Air.local>
2020-07-13 13:05:41 +0200

    updated kata

--- 6 Kyu/Your order, please/index.js
+++ 6 Kyu/Your order, please/index.js
@@ -1,3 +1,4 @@
 function order(words){
-    return words.split(' ').sort((a, b) => /[0-9]/.exec(a)[0] < /[0-9]/.exec(b)[0] ? -1 : 1).join(' ');
+    return words.split(' ').sort((a, b) => /\d/.exec(a)[0] < /\d/.exec(b)[0] ? -1 : 1).join(' ');
 }
+
