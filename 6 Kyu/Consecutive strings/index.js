d8f3550614de9808e6a0b1944e55cd2cedc679d8
Corrie Graham <corrie@Corries-MacBook-Air.local>
2020-07-14 09:03:34 +0200

    paused kata

--- 6 Kyu/Consecutive strings/index.js
+++ 6 Kyu/Consecutive strings/index.js
@@ -1,5 +1,14 @@
 function longestConsec(strarr, k) {
-    // your code
+    if (k > strarr.length || strarr.length === 0 || k <= 0) return ""
+    let longest = '';
+    let i = 0;
+
+    
+    // strarr.forEach((v, i, a) => `${v}${a[i++]}`.length > longest.length ? longest = `${v}${a[i++]}` : null);
+
+    return longest;
 }
 
-console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2));
+console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2), 'abigailtheta');
+console.log(longestConsec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1), "oocccffuucccjjjkkkjyyyeehh")
+console.log(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15), "\"\"");
